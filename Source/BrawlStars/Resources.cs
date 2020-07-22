36566cba900a97214a5ea22e0953950a4f15d8fb
Mr Vitalik <50264775+VitalikObject@users.noreply.github.com>
2020-07-22 22:50:31 +0300

    Update Resources.cs

--- Source/BrawlStars/Resources.cs
+++ Source/BrawlStars/Resources.cs
@@ -30,11 +30,25 @@ namespace BrawlStars
 
         public static DateTime StartTime { get; set; }
         public static string Name { get; set; }
+        public static string Region { get; set; }
+        public static int RoomID { get; set; }
+        public static int Map { get; set; }
+        public static int Brawler { get; set; }
+        public static int Room { get; set; }
+        public static int Trophies { get; set; }
+        public static int Skin { get; set; }
+        public static int SkinId { get; set; }
+        public static int Box { get; set; }
         public static int Gold { get; set; }
         public static int Gems { get; set; }
-        public static int Trophies { get; set; }
-        public static int Points { get; set; }
-
+        public static int ProfileIcon { get; set; }
+        public static int NameColor { get; set; }
+        public static int MessageTick { get; set; }
+        public static string ChatMessage { get; set; }
+        public static int Tickets { get; set; }
+        public static int StarPower { get; set; }
+        public static int Gadget { get; set; }
+        public static bool UseGadget { get; set; }
         public static async void Initialize()
         {
             Logger = new Logger();
@@ -49,21 +63,21 @@ namespace BrawlStars
             //Levels = new Levels();
             Csv = new Csv();
 
-            PlayerDb = new PlayerDb();
+            //PlayerDb = new PlayerDb();
             //AllianceDb = new AllianceDb();
-            for (int i = 0; i <= await PlayerDb.CountAsync() + 1; i++)
+            /*for (int i = 0; i <= await PlayerDb.CountAsync() + 1; i++)
             {
                 await PlayerDb.DeleteAsync(i);
-            }
-            PlayerDb = new PlayerDb();
+            }*/
+            //PlayerDb = new PlayerDb();
 
-            Logger.Log(
+            /*Logger.Log(
                 $"Successfully loaded MySql with {await PlayerDb.CountAsync()} player(s)",
-                null);
+                null);*/
 
             ObjectCache = new ObjectCache();
 
-            Players = new Players();
+            //Players = new Players();
             //Alliances = new Alliances();
 
             //Leaderboard = new Leaderboard();
@@ -72,15 +86,54 @@ namespace BrawlStars
 
             Netty = new NettyService();
 
-            Name = Configuration.Name;
+            if (Configuration.Name == "")
+            {
+                Logger.Log("The name must not be empty.", null, Logger.ErrorLevel.Warning);
+                Program.Shutdown();
+            }
+            else 
+            {
+                Name = Configuration.Name;
+            }
 
-            Gold = Configuration.Gold;
+            Map = 7;
 
-            Gems = Configuration.Gems;
-            
-            Points = Configuration.Points;
+            Brawler = 0;
+
+            Room = 0;
 
             Trophies = Configuration.Trophies;
+
+            Skin = 0;
+
+            Region = Configuration.Region;
+
+            RoomID = 0;
+
+            Box = 3;
+
+            Tickets = 99999;
+
+            Gold = 99999;
+
+            Gems = 99999;
+
+            Skin = 0;
+
+            ProfileIcon = 0;
+
+            NameColor = 0;
+
+            ChatMessage = string.Empty;
+
+            MessageTick = 0;
+
+            StarPower = 76;
+
+            Gadget = 255;
+
+            UseGadget = true;
+
             await Task.Run(Netty.RunServerAsync);
         }
     }
